891dc994663c04c0d31b000e6374d8c7d8335e98
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-04-22 22:01:52 +0000

    Add more markers to the map and resize the map zoom to make clusters less visible

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,19 +1,25 @@
 window.onload = function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6.5, 
+    let options = {
+        zoom: 7.25, 
         center: {
             lat: 41.2033, 
             lng: -77.1945,
-        }
-    });
+        },
+        mapId: "1a97cdea16f163b5",
+    };
+
+    let map = new google.maps.Map(document.getElementById("map"), options);
 
     var labels = 1234;
 
     var locations = [
+        {lat: 40.440624, lng: -79.995888},
         {lat: 39.906113, lng: -79.468056},
         {lat: 40.022899, lng: -78.517347},
         {lat: 39.8121605, lng: -77.2216721},
+        {lat: 40.259590, lng: -76.881866},
         {lat: 40.0213909, lng: -76.191827},
+        {lat: 39.9526, lng: -75.1652},
     ]
 
     var markers = locations.map(function(location, i) {
@@ -27,4 +33,5 @@ window.onload = function initMap() {
         imagePath:
           "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
       });
+
 }
