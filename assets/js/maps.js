ec2d2c466b3a08a25edbc140faa888b0b44aa297
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-04-24 21:05:53 +0000

    Add JS for multiple map markers and for infowindow on mouseover.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,37 +1,101 @@
-window.onload = function initMap() {
-    let options = {
+function initMap() {
+    const options = {
         zoom: 7.25, 
         center: {
             lat: 41.2033, 
             lng: -77.1945,
         },
-        mapId: "1a97cdea16f163b5",
+        mapId: "1a97cdea16f163b5",} 
+
+    const map = new google.maps.Map(document.getElementById("map"), options);
+
+
+    const markers = [
+        {coords: {lat: 40.440624, lng: -79.995888},  
+        content: '<h3>Pittsburgh</h3>',
+        info: 
+            `<h2>Test</h2>
+            <p>Test</p>
+            <a>Test</a>`
+        },
+
+        {coords: {lat: 39.906113, lng: -79.468056},
+        content: '<h3>Fallingwater</h3>',
+        info: 
+            `<h2>Test</h2>
+            <p>Test</p>
+            <a>Test</a>`
+        },
+
+        {coords: {lat: 40.022899, lng: -78.517347},
+        content: `<h3>Bedford</h3>`,
+        info: 
+            `<h2>Test</h2>
+            <p>Test</p>
+            <a>Test</a>`
+        },
+
+        {coords: {lat: 39.8121605, lng: -77.2216721},
+        content: `<h3>Gettysburg</h3>`,
+        info: 
+            `<h2>Test</h2>
+            <p>Test</p>
+            <a>Test</a>`
+        },
+
+        {coords: {lat: 40.259590, lng: -76.881866},
+        content: `<h3>Harrisburg</h3>`,
+        info: 
+            `<h2>Test</h2>
+            <p>Test</p>
+            <a>Test</a>`
+        },
+
+        {coords: {lat: 40.0213909, lng: -76.191827},
+        content: `<h3>Lancaster</h3>`,
+        info: 
+            `<h2>Test</h2>
+            <p>Test</p>
+            <a>Test</a>`
+        },
+
+        {coords: {lat: 39.9526, lng: -75.1652}, 
+        content: `<h3>Philadelphia</h3>`,
+        info: 
+            `<h2>Test</h2>
+            <p>Test</p>
+            <a>Test</a>`
+        },
+    ];
+
+    for(var i = 0; i< markers.length; i++){
+        addMarker(markers[i]);
     };
 
-    let map = new google.maps.Map(document.getElementById("map"), options);
+    function addMarker (props){
+        var marker = new google.maps.Marker({
+            position: props.coords, 
+            map: map,
+        });
 
-    var labels = 1234;
+        if(props.content){
+            var infoWindow = new google.maps.InfoWindow({
+              content:props.content
+            });
+  
+            marker.addListener('mouseover', function(){
+                infoWindow.open(map, marker);  
 
-    var locations = [
-        {lat: 40.440624, lng: -79.995888},
-        {lat: 39.906113, lng: -79.468056},
-        {lat: 40.022899, lng: -78.517347},
-        {lat: 39.8121605, lng: -77.2216721},
-        {lat: 40.259590, lng: -76.881866},
-        {lat: 40.0213909, lng: -76.191827},
-        {lat: 39.9526, lng: -75.1652},
-    ]
+                google.maps.event.addlistener(marker, 'mouseout', function(){
+                    infoWindow.close(map, marker); 
+                });
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location, 
-            label: labels[i % labels.length]
-        })
-    })
+            }); 
+            
+        
+        }  
+            
+    };  
+};
 
-    new MarkerClusterer(map, markers, {
-        imagePath:
-          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-      });
 
-}
