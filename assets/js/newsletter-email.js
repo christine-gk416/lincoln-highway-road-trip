1ff5090ab1332389f0c4acf0be4da4e8b969fc39
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-05-03 13:48:36 +0000

    Update JS file names to make sure all files are in lower case

---
+++ assets/js/newsletter-email.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => { 
+    document.getElementById('newsletterForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        emailjs.sendForm('gmail', 'newsletter', this)
+        .then(function() {
+            swal("Hey Road Tripper!", "You've signed up for our newsletter, so we'll send you updates on LH events", "success", {
+            button: "Thank you!",
+            });
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+
+        document.getElementById('newsletterForm').reset();     
+    });
+});
