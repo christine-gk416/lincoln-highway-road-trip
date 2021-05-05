9dd6d1436b0daa26264ad673dbab62b5a0648e0c
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-05-05 13:06:04 +0000

    Edit the JS code to be neater. Adjust the code to pull place icons on search

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,59 +1,79 @@
 function initAutocomplete() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 7.25, 
-        center: {
-            lat: 41.2033, 
-            lng: -77.1945,
-        },
-        mapId: "1a97cdea16f163b5",}); 
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7.25, 
+    center: {
+    lat: 41.2033, 
+    lng: -77.1945,},
+    mapId: "1a97cdea16f163b5",
+  }); 
 
-        var input = document.getElementById("map-search");
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+  let defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(39.23, -80.94),
+    new google.maps.LatLng(41.93, -75.32),
+  );   
 
-        map.addListener("bounds_changed", function() {
-            searchBox.setBounds(map.getBounds());
-        });
+  let options = {
+    bounds: defaultBounds,
+    types: ["geocode"],
+    componentRestrictions: {country: "us"}
+  };
+       
+  let input = document.getElementById("map-search");
+  let searchBox = new google.maps.places.SearchBox(input, options);
+
+  google.maps.event.addListener(map, "bounds_changed", function() {
+    searchBox.setBounds(map.getBounds());
+  });
     
-        var searchMarkers = [];
+  let searchMarkers = [];
     
-        searchBox.addListener("places_changed", function() {
-            var places = searchBox.getPlaces();
-            if(places.length == 0) {
-                return;
-            }
+  searchBox.addListener("places_changed", function() {
+    let places = searchBox.getPlaces();
+      if(places.length == 0) {
+        return;
+      }
     
-            searchMarkers.forEach(function(marker) {
-                marker.setMap(null);
-            });
+    searchMarkers.forEach(function(marker) {
+      marker.setMap(null);
+    });
     
-            searchMarkers = [];
+    searchMarkers = [];
     
-            var bounds = new google.maps.LatLngBounds();
-            places.forEach(function(place) {
-                if(!place.geometry) {
-                    return;
-                }
+    let bounds = new google.maps.LatLngBounds();
+
+    places.forEach(function(place) {
+      if(!place.geometry) {
+        return;
+      }
+
+        let icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
     
-                searchMarkers.push(new google.maps.Marker({
-                    map: map,
-                    position: place.geometry.location
-                }));
+        searchMarkers.push(new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        }));
             
-               if (place.geometry.viewport) {
-                   bounds.union(place.geometry.viewport);
-               } else {
-                   bounds.extend(place.geometry.location);
-               }
-            });
-            map.fitBounds(bounds);
-        });
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+          }
+    });
+      map.fitBounds(bounds);
+  });
 
-
-    const markers = [
-        {coords: {lat: 40.440624, lng: -79.995888},  
-        content: '<h3>Pittsburgh</h3><p>Start here! &#128664</p>',
-        info: 
+  const markers = [
+    {coords: {lat: 40.440624, lng: -79.995888}, 
+      content: '<h3>Pittsburgh</h3><p>Start here! &#128664</p>',
+      info: 
             `<h2>Pittsburgh</h2>
             <br>
             <p>Sit amet, consectetur adipiscing elit. 
@@ -65,100 +85,94 @@ function initAutocomplete() {
             Aliquam volutpat erat id elit fringilla ullamcorper. </p>
             <br>
             <a href="#Pittsburgh" class="read-more-btn">More info</a>`
-        },
+    },
 
-        {coords: {lat: 39.906113, lng: -79.468056},
-        content: '<h3>Fallingwater</h3>',
-        info: 
+    {coords: {lat: 39.906113, lng: -79.468056},
+      content: '<h3>Fallingwater</h3>',
+      info: 
             `<h2>Fallingwater</h2>
             <p>Test</p>
             <a href="#Fallingwater" class="read-more-btn">More info</a>`
-        },
+    },
 
-        {coords: {lat: 40.022899, lng: -78.517347},
-        content: `<h3>Bedford</h3>`,
-        info: 
+    {coords: {lat: 40.022899, lng: -78.517347},
+      content: `<h3>Bedford</h3>`,
+      info: 
             `<h2>Bedford</h2>
             <p>Test</p>
             <a href="#Bedford" class="read-more-btn">More info</a>`
-        },
+    },
 
-        {coords: {lat: 39.8121605, lng: -77.2216721},
-        content: `<h3>Gettysburg</h3><p>&#128123</p>`,
-        info: 
+    {coords: {lat: 39.8121605, lng: -77.2216721},
+      content: `<h3>Gettysburg</h3><p>&#128123</p>`,
+      info: 
             `<h2>Gettysburg</h2>
             <p>Test</p>
             <a href="#Gettysburg" class="read-more-btn">More info</a>`
-        },
+    },
 
-        {coords: {lat: 40.259590, lng: -76.881866},
-        content: `<h3>Harrisburg</h3>`,
-        info: 
+    {coords: {lat: 40.259590, lng: -76.881866},
+      content: `<h3>Harrisburg</h3>`,
+      info: 
             `<h2>Harrisburg</h2>
             <p>Test</p>
             <a href="#Harrisburg" class="read-more-btn">More info</a>`
-        },
+    },
 
-        {coords: {lat: 40.0213909, lng: -76.191827},
-        content: `<h3>Lancaster</h3>`,
-        info: 
+    {coords: {lat: 40.0213909, lng: -76.191827},
+      content: `<h3>Lancaster</h3>`,
+      info: 
             `<h2>Lancaster</h2>
             <p>Test</p>
             <a href="#Lancaster" class="read-more-btn">More info</a>`
-        },
+    },
 
-        {coords: {lat: 39.9526, lng: -75.1652}, 
-        content: `<h3>Philadelphia</h3><p>End here &#128075</p>`,
-        info: 
+    {coords: {lat: 39.9526, lng: -75.1652}, 
+      content: `<h3>Philadelphia</h3><p>End here &#128075</p>`,
+      info: 
             `<h2>Philadelphia</h2>
             <p>Test</p>
             <a href="#Philadelphia" class="read-more-btn">More info</a>`
-        },
-    ];
+    },
+  ];
 
-    for(var i = 0; i< markers.length; i++){
-        addMarker(markers[i]);
-    };
+  for(var i = 0; i< markers.length; i++){
+    addMarker(markers[i]);
+  };
 
-    function addMarker (props){
-        var image = 'http://maps.google.com/mapfiles/kml/paddle/red-stars.png';
-        var marker = new google.maps.Marker({
-            position: props.coords, 
-            icon: image,
-            map: map  
-        });
+  function addMarker (props){
+    let image = 'http://maps.google.com/mapfiles/kml/paddle/red-stars.png';
+    let marker = new google.maps.Marker({
+      position: props.coords, 
+      icon: image,
+      map: map  
+  });
 
-        if(props.content){
-            var infoWindow = new google.maps.InfoWindow({
-              content: props.content
-            });
+  if(props.content){
+    var infoWindow = new google.maps.InfoWindow({
+      content: props.content
+  });
   
-            marker.addListener('mouseover', function(){
-                infoWindow.open(map, marker);  
+  marker.addListener('mouseover', function(){
+    infoWindow.open(map, marker);  
+  }); 
 
-            }); 
+  marker.addListener('mouseout', function(){
+    infoWindow.close(map, marker);  
+  });
 
-            marker.addListener('mouseout', function(){
-                infoWindow.close(map, marker);  
+  google.maps.event.addListener(marker, 'click', (function(i) {
+    return function() {
+      document.getElementById('sideCard').innerHTML = markers[i].info;
+      }
+      })(i));
 
-            });
-
-            google.maps.event.addListener(marker, 'click', (function(i) {
-                return function() {
-                    document.getElementById('sideCard').innerHTML = markers[i].info;
-                }
-            })(i));
-
-        
-        }    
-            
-           
-    };
-    
+    }    
+  };
 };
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    initAutocomplete();
-  });
+  initAutocomplete();
+});
 
 
