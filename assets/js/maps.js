be665ed34659b8b13c079df5a52929c99a5ef1b5
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-05-06 20:39:37 +0000

    Adjust map bounds to keep the searchbox set to the map area

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,14 +2,14 @@ function initAutocomplete() {
   let map = new google.maps.Map(document.getElementById("map"), {
     zoom: 7.25, 
     center: {
-    lat: 41.2033, 
-    lng: -77.1945,},
+    lat: 40.58, 
+    lng: -77.57,},
     mapId: "1a97cdea16f163b5",
   }); 
 
   let defaultBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(39.23, -80.94),
-    new google.maps.LatLng(41.93, -75.32),
+    new google.maps.LatLng(39.71, -80.51),
+    new google.maps.LatLng(40.51, -74.41),
   );   
 
   let options = {
@@ -67,12 +67,12 @@ function initAutocomplete() {
             bounds.extend(place.geometry.location);
           }
     });
-      map.fitBounds(bounds);
+    map.fitBounds(bounds);
   });
 
   const markers = [
-    {coords: {lat: 40.440624, lng: -79.995888}, 
-      content: '<h3>Pittsburgh</h3><p>Start here! &#128664</p>',
+    {coords: {lat: 40.44, lng: -79.99}, 
+      content: "<h3>Pittsburgh</h3><p>Start here! &#128664</p>",
       info: 
             `<h2>Pittsburgh</h2>
             <br>
@@ -87,8 +87,8 @@ function initAutocomplete() {
             <a href="#Pittsburgh" class="read-more-btn">More info</a>`
     },
 
-    {coords: {lat: 39.906113, lng: -79.468056},
-      content: '<h3>Fallingwater</h3>',
+    {coords: {lat: 39.906, lng: -79.467},
+      content: "<h3>Fallingwater</h3>",
       info: 
             `<h2>Fallingwater</h2>
             <p>Test</p>
@@ -96,7 +96,7 @@ function initAutocomplete() {
     },
 
     {coords: {lat: 40.022899, lng: -78.517347},
-      content: `<h3>Bedford</h3>`,
+      content: "<h3>Bedford</h3>",
       info: 
             `<h2>Bedford</h2>
             <p>Test</p>
@@ -104,7 +104,7 @@ function initAutocomplete() {
     },
 
     {coords: {lat: 39.8121605, lng: -77.2216721},
-      content: `<h3>Gettysburg</h3><p>&#128123</p>`,
+      content: "<h3>Gettysburg</h3><p>&#128123</p>",
       info: 
             `<h2>Gettysburg</h2>
             <p>Test</p>
@@ -112,15 +112,15 @@ function initAutocomplete() {
     },
 
     {coords: {lat: 40.259590, lng: -76.881866},
-      content: `<h3>Harrisburg</h3>`,
+      content: "<h3>Harrisburg</h3>",
       info: 
             `<h2>Harrisburg</h2>
             <p>Test</p>
             <a href="#Harrisburg" class="read-more-btn">More info</a>`
     },
 
-    {coords: {lat: 40.0213909, lng: -76.191827},
-      content: `<h3>Lancaster</h3>`,
+    {coords: {lat: 40.03, lng: -76.30},
+      content: "<h3>Lancaster</h3>",
       info: 
             `<h2>Lancaster</h2>
             <p>Test</p>
@@ -128,7 +128,7 @@ function initAutocomplete() {
     },
 
     {coords: {lat: 39.9526, lng: -75.1652}, 
-      content: `<h3>Philadelphia</h3><p>End here &#128075</p>`,
+      content: "<h3>Philadelphia</h3><p>End here &#128075</p>",
       info: 
             `<h2>Philadelphia</h2>
             <p>Test</p>
@@ -141,7 +141,7 @@ function initAutocomplete() {
   };
 
   function addMarker (props){
-    let image = 'http://maps.google.com/mapfiles/kml/paddle/red-stars.png';
+    let image = "http://maps.google.com/mapfiles/kml/paddle/red-stars.png";
     let marker = new google.maps.Marker({
       position: props.coords, 
       icon: image,
@@ -153,17 +153,17 @@ function initAutocomplete() {
       content: props.content
   });
   
-  marker.addListener('mouseover', function(){
+  marker.addListener("mouseover", function(){
     infoWindow.open(map, marker);  
   }); 
 
-  marker.addListener('mouseout', function(){
+  marker.addListener("mouseout", function(){
     infoWindow.close(map, marker);  
   });
 
-  google.maps.event.addListener(marker, 'click', (function(i) {
+  google.maps.event.addListener(marker, "click", (function(i) {
     return function() {
-      document.getElementById('sideCard').innerHTML = markers[i].info;
+      document.getElementById("sideCard").innerHTML = markers[i].info;
       }
       })(i));
 
@@ -174,5 +174,3 @@ function initAutocomplete() {
 document.addEventListener("DOMContentLoaded", function(event) {
   initAutocomplete();
 });
-
-
