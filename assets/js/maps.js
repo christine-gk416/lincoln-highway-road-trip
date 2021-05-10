305b140a5c622558ff163459996d14a001e483f6
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-05-10 21:19:55 +0000

    Add new Search page to other pages on site. Remove Search JS from the Map JS

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
-function initAutocomplete() {
+function initMap() {
   let map = new google.maps.Map(document.getElementById("map"), {
     zoom: 7.25, 
     center: {
@@ -6,75 +6,11 @@ function initAutocomplete() {
     lng: -77.57,},
     mapId: "1a97cdea16f163b5",
   }); 
-
-  let defaultBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(39.71, -80.51),
-    new google.maps.LatLng(40.51, -74.41),
-  );   
-
-  let options = {
-    bounds: defaultBounds,
-    types: ["geocode"],
-    componentRestrictions: {country: "us"}
-  };
-       
-  let input = document.getElementById("map-search");
-  let searchBox = new google.maps.places.SearchBox(input, options);
-
-  google.maps.event.addListener(map, "bounds_changed", function() {
-    searchBox.setBounds(map.getBounds());
-  });
-    
-  let searchMarkers = [];
-    
-  searchBox.addListener("places_changed", function() {
-    let places = searchBox.getPlaces();
-      if(places.length == 0) {
-        return;
-      }
-    
-    searchMarkers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    
-    searchMarkers = [];
-    
-    let bounds = new google.maps.LatLngBounds();
-
-    places.forEach(function(place) {
-      if(!place.geometry) {
-        return;
-      }
-
-        let icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25),
-        };
-    
-        searchMarkers.push(new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        }));
-            
-      if (place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-        } else {
-            bounds.extend(place.geometry.location);
-          }
-    });
-    map.fitBounds(bounds);
-  });
-
   const markers = [
-    {coords: {lat: 40.44, lng: -79.99}, 
+    {
+      coords: { lat: 40.44, lng: -79.99 },
       content: "<h3>Pittsburgh</h3><p>Start here! &#128664</p>",
-      info: 
-            `<h2>Pittsburgh</h2>
+      info: `<h2>Pittsburgh</h2>
             <p>Start your trip in Pittsburgh, the second largest city in Pennsylvania. 
             This city is located in the far southwestern part of the state, on the Allegheny plateau. 
             Many of its distinctive neighborhoods are built right into the these mountainous foothills. 
@@ -86,13 +22,13 @@ function initAutocomplete() {
             It’s regularly listed as the most livable city in the US. 
             </p>
             <br>
-            <a href="#Pittsburgh" class="read-more-btn">More info</a>`
+            <a href="#Pittsburgh" class="read-more-btn">More info</a>`,
     },
 
-    {coords: {lat: 39.906, lng: -79.467},
+    {
+      coords: { lat: 39.906, lng: -79.467 },
       content: "<h3>Fallingwater</h3>",
-      info: 
-            `<h2>Fallingwater</h2>
+      info: `<h2>Fallingwater</h2>
             <p>This iconic house was built by one of America’s most famous architects. 
             Fallingwater was built between 1933-38 for the Kaufmann family, 
             the owners of a prominent department store chain in Pittsburgh. 
@@ -106,13 +42,13 @@ function initAutocomplete() {
             In 2019, Fallingwater house was added to the list of UNESCO world heritage sites. 
             </p>
             <br>
-            <a href="#Fallingwater" class="read-more-btn">More info</a>`
+            <a href="#Fallingwater" class="read-more-btn">More info</a>`,
     },
 
-    {coords: {lat: 40.022899, lng: -78.517347},
+    {
+      coords: { lat: 40.022899, lng: -78.517347 },
       content: "<h3>Bedford</h3>",
-      info: 
-            `<h2>Bedford</h2>
+      info: `<h2>Bedford</h2>
             <p>
             Bedford is a small town tucked into the Allegheny mountains. 
             If you’re there in the autumn, the trees will be alive with changing colours and there’s a fall foliage festival to celebrate. 
@@ -128,13 +64,13 @@ function initAutocomplete() {
             It’s closed now, but is still full of potential for photographs.
             </p>
             <br>
-            <a href="#Bedford" class="read-more-btn">More info</a>`
+            <a href="#Bedford" class="read-more-btn">More info</a>`,
     },
 
-    {coords: {lat: 39.8121605, lng: -77.2216721},
+    {
+      coords: { lat: 39.8121605, lng: -77.2216721 },
       content: "<h3>Gettysburg</h3><p>&#128123</p>",
-      info: 
-            `<h2>Gettysburg</h2>
+      info: `<h2>Gettysburg</h2>
             <p>
             The site of one of the bloodiest and northern-most battles of the American Civil War, is a must-see to learn about the darker parts of American history. 
             Gettysburg town is situated down the road from the battlefield, which makes it a hot-spot for ghosts. 
@@ -147,13 +83,13 @@ function initAutocomplete() {
             The town also has a robust shopping and antiques district, as well as wine/craft beer and food tours. 
             </p>
             <br>
-            <a href="#Gettysburg" class="read-more-btn">More info</a>`
+            <a href="#Gettysburg" class="read-more-btn">More info</a>`,
     },
 
-    {coords: {lat: 40.259590, lng: -76.881866},
+    {
+      coords: { lat: 40.25959, lng: -76.881866 },
       content: "<h3>Harrisburg</h3>",
-      info: 
-            `<h2>Harrisburg</h2>
+      info: `<h2>Harrisburg</h2>
             <p>
             The capitol city and the third largest in Pennsylvania. 
             Harrisburg is built along the Susquehanna river, a wide and enchanting river that flows into the Chesapeake Bay. 
@@ -167,61 +103,64 @@ function initAutocomplete() {
             The keystone of midtown is the Midtown Scholar Bookshop, which is run by the city’s mayor. 
             </p>
             <br>
-            <a href="#Harrisburg" class="read-more-btn">More info</a>`
+            <a href="#Harrisburg" class="read-more-btn">More info</a>`,
     },
 
-    {coords: {lat: 40.03, lng: -76.30},
+    {
+      coords: { lat: 40.03, lng: -76.3 },
       content: "<h3>Lancaster</h3>",
-      info: 
-            `<h2>Lancaster</h2>
+      info: `<h2>Lancaster</h2>
             <p>Test</p>
-            <a href="#Lancaster" class="read-more-btn">More info</a>`
+            <a href="#Lancaster" class="read-more-btn">More info</a>`,
     },
 
-    {coords: {lat: 39.9526, lng: -75.1652}, 
+    {
+      coords: { lat: 39.9526, lng: -75.1652 },
       content: "<h3>Philadelphia</h3><p>End here &#128075</p>",
-      info: 
-            `<h2>Philadelphia</h2>
+      info: `<h2>Philadelphia</h2>
             <p>Test</p>
-            <a href="#Philadelphia" class="read-more-btn">More info</a>`
+            <a href="#Philadelphia" class="read-more-btn">More info</a>`,
     },
   ];
 
-  for(var i = 0; i< markers.length; i++){
+  for (var i = 0; i < markers.length; i++) {
     addMarker(markers[i]);
-  };
+  }
 
-  function addMarker (props){
-    let image = "http://maps.google.com/mapfiles/kml/paddle/red-stars.png";
+  function addMarker(props) {
+    let image = "https://maps.google.com/mapfiles/kml/paddle/red-stars.png";
     let marker = new google.maps.Marker({
-      position: props.coords, 
+      position: props.coords,
       icon: image,
-      map: map  
-  });
+      map: map,
+    });
 
-  if(props.content){
-    var infoWindow = new google.maps.InfoWindow({
-      content: props.content
-  });
-  
-  marker.addListener("mouseover", function(){
-    infoWindow.open(map, marker);  
-  }); 
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content,
+      });
 
-  marker.addListener("mouseout", function(){
-    infoWindow.close(map, marker);  
-  });
+      marker.addListener("mouseover", function () {
+        infoWindow.open(map, marker);
+      });
 
-  google.maps.event.addListener(marker, "click", (function(i) {
-    return function() {
-      document.getElementById("sideCard").innerHTML = markers[i].info;
-      }
-      })(i));
+      marker.addListener("mouseout", function () {
+        infoWindow.close(map, marker);
+      });
 
-    }    
-  };
-};
+      google.maps.event.addListener(
+        marker,
+        "click",
+        (function (i) {
+          return function () {
+            document.getElementById("sideCard").innerHTML = markers[i].info;
+          };
+        })(i)
+      );
+    }
+  }
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  initAutocomplete();
+document.addEventListener("DOMContentLoaded", function (event) {
+  initMap();
 });
